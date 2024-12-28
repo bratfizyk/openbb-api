c0916afadc2608a5b056af24a79a3c87f6993724
Alojzy Leszcz <aleszcz@pm.me>
2024-12-28 23:53:29 +0100

    Initial commit

---
+++ src/OpenBBAPI.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module OpenBBAPI where
+
+import Data.Aeson (FromJSON)
+import Data.List (intercalate)
+import Data.Proxy (Proxy(..))
+import Data.Text (Text, pack)
+import GHC.Generics (Generic)
+import Network.HTTP.Client (newManager, defaultManagerSettings)
+import Servant.API (ToHttpApiData(toUrlPiece), JSON, Required, QueryParam', type (:>), Get)
+import Servant.Client (client, mkClientEnv, runClientM, ClientM, BaseUrl(BaseUrl), Scheme(Http))
+
+type Provider = String
+type Ticker = String
+
+instance ToHttpApiData [Ticker] where
+  toUrlPiece :: [Ticker] -> Text
+  toUrlPiece = pack . intercalate ","
+
+data EquityQuoteResponse = EquityQuoteResponse
+  { symbol :: String
+  , asset_type :: String
+  , name :: String
+  , currency :: String
+  , last_price :: Float
+  , open :: Float
+  , high :: Float
+  , low :: Float
+  , volume :: Int
+  }
+  deriving (Show, Generic)
+
+newtype ApiResult a = ApiResult
+  {
+    results :: [a]
+  }
+  deriving (Show, Generic)
+
+instance FromJSON EquityQuoteResponse
+instance FromJSON a => FromJSON (ApiResult a)
+
+type EquityAPI = "equity" :> "price" :> "quote" :> QueryParam' '[Required] "provider" Provider :> QueryParam' '[Required] "symbol" [Ticker] :> Get '[JSON] (ApiResult EquityQuoteResponse)
+type API = "api" :> "v1" :> EquityAPI
+
+api :: Proxy API
+api = Proxy
+
+equity :: Provider -> [Ticker] -> ClientM (ApiResult EquityQuoteResponse)
+equity = client api
+
+equityQuote :: [Ticker] -> ClientM (ApiResult EquityQuoteResponse)
+equityQuote tickers = do equity "yfinance" tickers
+
+runQuery :: ClientM a -> IO a
+runQuery req = do
+  manager' <- newManager defaultManagerSettings
+  res <- runClientM req (mkClientEnv manager' (BaseUrl Http "localhost" 6900 ""))
+  case res of
+    Left err -> error $ "Error: " ++ show err
+    Right eq -> return eq
